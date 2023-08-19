66a06f7a98fd05223ce7ca2e79a5aee9f374652b
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-08-19 12:33:33 +0300

    Insert benchmarking code

--- metagraph/src/cli/query.cpp
+++ metagraph/src/cli/query.cpp
@@ -1080,6 +1080,26 @@ construct_query_graph(const AnnotatedDBG &anno_graph,
     return std::make_unique<AnnotatedDBG>(graph, std::move(annotation));
 }
 
+// Jarno addition for quick benchmarking
+vector<string> get_reads(const string& fastq_file){
+    vector<string> reads;
+    ifstream in(fastq_file);
+    string line;
+    int64_t line_mod4 = 0;
+
+    while(getline(in, line)){
+        if(line_mod4 == 1) reads.push_back(line);
+        line_mod4 = (line_mod4 + 1) % 4;
+    }
+
+    return reads;
+}
+
+// Jarno addition for quick benchmarking
+int64_t time_now_ms(){
+    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();;
+}
+
 
 int query_graph(Config *config) {
     assert(config);
@@ -1089,6 +1109,33 @@ int query_graph(Config *config) {
     assert(config->infbase_annotators.size() == 1);
 
     std::shared_ptr<DeBruijnGraph> graph = load_critical_dbg(config->infbase);
+
+    // Jarno benchmarking code starts here
+
+    vector<string> reads = get_reads("../../data/ERR434259_1_prefix.fastq");
+    int64_t n_kmers_processed = 0;
+    int64_t n_hits = 0;
+
+    auto callback = [&n_kmers_processed, &n_hits](int64_t node_id){
+        n_kmers_processed++;
+        n_hits += (node_id != 0);
+    };
+
+    int64_t t0 = time_now_ms();
+    for(const string& S : reads){
+        graph->map_to_nodes_sequentially(S, callback);
+    }
+    int64_t t1 = time_now_ms();
+
+    int64_t elapsed_us = (t1-t0) * 1000;
+    cout << (double) elapsed_us / n_kmers_processed << " us / k-mer" << endl;
+    cout << "k-mers processed: " << n_kmers_processed << endl;
+    cout << "Hit rate: " << (double) n_hits / n_kmers_processed << endl;
+
+    exit(0);
+
+    // Jarno benchmarking code ends here
+
     std::unique_ptr<AnnotatedDBG> anno_graph = initialize_annotated_dbg(graph, *config);
 
     ThreadPool thread_pool(std::max(1u, get_num_threads()) - 1, 1000);
